ad6fc3358bc91449ba50b299ae8e0f7e23d48375
Swan-17 <matthewswan17@gmail.com>
2026-09-08 14:27:54 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,25 @@
-const CACHE_NAME = 'beer-tracker-v1';
+const CACHE_NAME = 'beer-tracker-v2'; // Changed version to force cache update
 const urlsToCache = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', event => {
+  self.skipWaiting(); // Instantly activate the new service worker
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', event => {
+  // Clear out old caches (like beer-tracker-v1)
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    }).then(() => self.clients.claim()) // Claim clients immediately
   );
 });
 
