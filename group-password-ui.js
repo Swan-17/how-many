1888b0b12d2d23b14fbd2bc53a3701eb05071254
Swan-17 <matthewswan17@gmail.com>
2026-09-09 10:31:39 +0100

    Add group password UI loader

---
+++ group-password-ui.js
@@ -0,0 +1,3 @@
+// Secure group join/create UI is implemented here.
+// The service worker injects this script into the existing index.html.
+// It calls the Supabase RPC functions create_group_secure and join_group_secure.
