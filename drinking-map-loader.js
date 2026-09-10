3ba76a0cbaeb700f9d239ef76c11e6586f0bf72b
Swan-17 <matthewswan17@gmail.com>
2026-09-10 16:09:23 +0100

    Load top venues stats feature

--- drinking-map-loader.js
+++ drinking-map-loader.js
@@ -7,7 +7,15 @@
     const s = document.createElement('script');
     s.src = './drinking-map.js?test=' + Date.now();
     s.async = false;
-    s.onload = () => console.log('Drinking map test script loaded');
+    s.onload = () => {
+      console.log('Drinking map test script loaded');
+      const venues = document.createElement('script');
+      venues.src = './top-venues.js?test=' + Date.now();
+      venues.async = false;
+      venues.onload = () => console.log('Top venues stats script loaded');
+      venues.onerror = () => console.error('Top venues stats script failed to load');
+      document.head.appendChild(venues);
+    };
     s.onerror = () => console.error('Drinking map test script failed to load');
     document.head.appendChild(s);
   };
